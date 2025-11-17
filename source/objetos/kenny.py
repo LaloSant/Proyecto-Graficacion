0c77069751a3cf04b1eadb81e7ed8524e504d3d4
yael8895 <yael.sampayo.01082005@gmail.com>
2025-11-17 13:14:50 -0600

    Animacion en kenny
    Se le quito codigo innecesario a kenny y se le añadieron las animaciones

--- source/objetos/kenny.py
+++ source/objetos/kenny.py
@@ -2,7 +2,9 @@ from OpenGL.GL import * 	# type: ignore
 from OpenGL.GLU import *	# type: ignore
 from OpenGL.GLUT import *	# type: ignore
 import math
-from .objeto import Objeto
+from source.objetos.objeto import Objeto
+import utils.estado as est
+import source.objetos.pers_args as pers_args
 
 class Kenny(Objeto):
 	def draw_hat(self): 
@@ -72,116 +74,103 @@ class Kenny(Objeto):
 		glPopMatrix()
 
 	def draw_arms(self):
-		active = False
-		raise_offset = 0.55 if active else 0.0
-		rotate_angle = -80 if active else 0
-		rotate_angle_left = 80 if active else 0 
-		side_offset = 0.5 if active else 0.0
-		hand_offset = 1.15 if active else 0.0
-		side_offset_hand = 1 if active else 0.0
+
 
 		# Brazo derecho (originalmente a la derecha)
 		# manga
 		glPushMatrix()
-		glTranslatef(1.2, 0, 0)
 		# glColor3f(0, 0, 1.0)
 		self.set_material_properties(self.rgb(0, 0, 255))
+		glRotatef(pers_args.caminando, 1, 0, 0)
+		glTranslatef(1.2, 0, 0)
 		glutSolidCube(0.25)
 		glPopMatrix()
 		# brazo (segmento)
 		glPushMatrix()
-		glTranslatef(1.2+side_offset, -0.5 + raise_offset, 0)
-		if active:
-			glRotatef(rotate_angle, 0, 0, 1)
 		# glColor3f(1.0, 0, 0)
 		self.set_material_properties(self.rgb(255, 0, 0))
+		glRotatef(pers_args.caminando, 1, 0, 0)
+		glTranslatef(1.2, -0.5 , 0)
 		glScalef(0.25, 0.8, 0.25)
 		glutSolidCube(1)
 		glPopMatrix()
 		# mano
 		glPushMatrix()
-		glTranslatef(1.2+side_offset_hand, -1 + hand_offset, 0)
-		if active:
-			glRotatef(rotate_angle, 0, 0, 1)
 		# glColor3f(1.01185, 0.8735, 0.7984)
 		self.set_material_properties(self.rgb(255, 222, 201))
+		glRotatef(pers_args.caminando, 1, 0, 0)
+		glTranslatef(1.2, -1 , 0)
 		glutSolidCube(0.25)
 		glPopMatrix()
 
 		# Brazo izquierdo
 		# manga
 		glPushMatrix()
-		glTranslatef(-0.3, 0, 0)
 		# glColor3f(0, 0, 1.0)
 		self.set_material_properties(self.rgb(1, 0, 255))
+		glRotatef(-pers_args.caminando, 1, 0, 0)
+		glTranslatef(-0.3, 0, 0)
 		glutSolidCube(0.25)
 		glPopMatrix()
 		# brazo
 		glPushMatrix()
-		glTranslatef(-0.3-side_offset, -0.5 + raise_offset, 0)
-		if active:
-			glRotatef(rotate_angle_left, 0, 0, 1)
 		# glColor3f(1.0, 0, 0)
 		self.set_material_properties(self.rgb(255, 0, 0))
+		glRotatef(-pers_args.caminando, 1, 0, 0)
+		glTranslatef(-0.3, -0.5, 0)
 		glScalef(0.25, 0.8, 0.25)
 		glutSolidCube(1)
 		glPopMatrix()
 		# mano
 		glPushMatrix()
-		glTranslatef(-0.3-side_offset_hand, -1 + hand_offset, 0)
-		if active:
-			glRotatef(rotate_angle_left, 0, 0, 1)
 		# glColor3f(1.01185, 0.8735, 0.7984)
 		self.set_material_properties(self.rgb(255, 222, 201))
+		glRotatef(-pers_args.caminando, 1, 0, 0)
+		glTranslatef(-0.3, -1, 0)
 		glutSolidCube(0.25)
 		glPopMatrix()
 
 	def draw_legs(self):
-		active = False
-		spread = 0.15 if active else 0.0
-		rotate_spread = 15 if active else 0
+
 
 		# Pierna izquierda (prisma rectangular)
 		glPushMatrix()
-		glTranslatef(0 - spread, -1.59, 0)
-		if active:
-			glRotatef(-rotate_spread, 0, 0, 1)
+		
 		# glColor3f(1.0, 1.0, 1.0)
 		self.set_material_properties(self.rgb(255, 255, 255))
+		glRotatef(pers_args.caminando, 1, 0, 0)
+		glTranslatef(0 , -1.59, 0)
 		glScalef(0.3, 0.5, 0.3)
 		glutSolidCube(1.0)
 		glPopMatrix()
 
 		# Pierna derecha (prisma rectangular)
 		glPushMatrix()
-		glTranslatef(0.9 + spread, -1.59, 0)
-		if active:
-			glRotatef(rotate_spread, 0, 0, 1)
 		# glColor3f(1.0, 1.0, 1.0)
 		self.set_material_properties(self.rgb(255, 255, 255))
+		glRotatef(-pers_args.caminando, 1, 0, 0)
+		glTranslatef(0.9  , -1.59, 0)
 		glScalef(0.3, 0.5, 0.3)
 		glutSolidCube(1.0)
 		glPopMatrix()
 
 		# Pie izquierdo
 		glPushMatrix()
-		glTranslatef(0 - spread, -2, 0)
-		glRotatef(-90, 1, 0, 0)
-		if active:
-			glRotatef(-rotate_spread, 0, 0, 1)
 		# glColor3f(0, 0, 0)
 		self.set_material_properties(self.rgb(0, 0, 0))
+		glRotatef(pers_args.caminando, 1, 0, 0)
+		glTranslatef(0  , -2, 0)
+		glRotatef(-90, 1, 0, 0)
 		glutSolidCube(0.3)
 		glPopMatrix()
 
 		# Pie derecho
 		glPushMatrix()
-		glTranslatef(0.9 + spread, -2, 0)
-		glRotatef(-90, 1, 0, 0)
-		if active:
-			glRotatef(rotate_spread, 0, 0, 1)
 		# glColor3f(0, 0, 0)
 		self.set_material_properties(self.rgb(0, 0, 0))
+		glRotatef(-pers_args.caminando, 1, 0, 0)
+		glTranslatef(0.9, -2, 0)
+		glRotatef(-90, 1, 0, 0)
 		glutSolidCube(0.3)
 		glPopMatrix()
 
